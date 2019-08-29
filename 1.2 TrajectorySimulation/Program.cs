7ede1b328fe0db6fa842ece9fed2cfe3f3ed5855
Саша Волков <Lexvolk2004@yandex.ru>
2019-08-29 23:27:42 +0400

    Adding task1.2

---
+++ 1.2 TrajectorySimulation/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._2_TrajectorySimulation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Object> currentObjects = new List<Object>(new[] { new Object(5, 5), new Object(10, 10), new Object(15, 15) });
+
+            while (true)
+            {
+                DetectCollisions(currentObjects.ToArray());
+                MoveObjects(currentObjects.ToArray());
+                RenderObjects(currentObjects.ToArray());
+            }
+        }
+
+        private static void RenderObjects(Object[] objects)
+        {
+            for (int i = 0; i < objects.Length; i++)
+            {
+                if (objects[i].IsDead)
+                    continue;
+
+                Console.SetCursorPosition(objects[i].X, objects[i].Y);
+                Console.Write(i + 1);
+            }
+        }
+
+        private static void MoveObjects(Object[] objects)
+        {
+            Random random = new Random();
+
+            foreach (var obj in objects)
+                obj.Move(random.Next(-1, 1), random.Next(-1, 1));
+        }
+
+        private static void DetectCollisions(Object[] objects)
+        {
+            for (int i = 0; i < objects.Length; i++)
+            {
+                if (objects[i].IsDead)
+                    continue;
+
+                for (int j = 0; j < objects.Length; j++)
+                {
+                    if (j == i || objects[j].IsDead)
+                        continue;
+
+                    if (IsCollided(objects[i], objects[j]))
+                    {
+                        objects[i].Kill();
+                        objects[j].Kill();
+                    }
+                }
+            }
+        }
+
+        private static bool IsCollided(Object object1, Object object2) => object1.X == object2.X && object1.Y == object2.Y;
+    }
+
+    class Object
+    {
+        public bool IsDead => !_isAlive;
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        private bool _isAlive = true;
+
+        public Object(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Move(int xDelta, int yDelta)
+        {
+            if (!_isAlive)
+                return;
+
+            X += xDelta;
+            Y += yDelta;
+
+            if (X < 0)
+                X = 0;
+
+            if (Y < 0)
+                Y = 0;
+        }
+
+        public void Kill() => _isAlive = false;
+    }
+}
