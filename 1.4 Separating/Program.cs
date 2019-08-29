c8d28a881e8a53556c00aeeb0cb92b0c6e02caac
Саша Волков <Lexvolk2004@yandex.ru>
2019-08-30 00:07:53 +0400

    Adding task 1.4

---
+++ 1.4 Separating/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _1._4_Separating
+{
+    class Player
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        private Mover _mover;
+        private Weapon _weapon;
+
+        public Player(Mover mover, Weapon weapon)
+        {
+            _mover = mover;
+            _weapon = weapon;
+        }
+
+        public void Move() => _mover.Move();
+        public void Attack() => _weapon.Attack();
+    }
+
+    class Mover
+    {
+        public float MovementDirectionX { get; private set; }
+        public float MovementDirectionY { get; private set; }
+        public float MovementSpeed { get; private set; }
+
+        public void Move()
+        {
+            //Do move
+        }
+    }
+
+    class Weapon
+    {
+        public float WeaponCooldown { get; private set; }
+        public int WeaponDamage { get; private set; }
+
+        public void Attack()
+        {
+            //attack
+        }
+
+        public bool IsReloading()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
