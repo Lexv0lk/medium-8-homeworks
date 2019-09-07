7960f6b210a35ea004480ffc7f50436fd4df4473
Саша Волков <Lexvolk2004@yandex.ru>
2019-09-08 03:47:52 +0400

    Adding product

---
+++ 2.4 ProductWithDiscount/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _2._4_ProductWithDiscount
+{
+    class Product
+    {
+        public string Name { get; private set; }
+        public bool HasDiscount { get; private set; }
+
+        public bool CanBeDelivered => !HasDiscount;
+
+        private float _price;
+        private int _discountPercents;
+
+        public float GetPrice() => _price;
+        public float GetPriceWithDiscount() => _price - _discountPercents / 100f * _price;
+
+        public void SetDiscount(int percents)
+        {
+            if (percents < 0 || percents > 100)
+                throw new ArgumentOutOfRangeException();
+
+            HasDiscount = true;
+            _discountPercents = percents;
+        }
+
+        public void RemoveDiscount()
+        {
+            HasDiscount = false;
+            _discountPercents = 0;
+        }
+    }
+}
