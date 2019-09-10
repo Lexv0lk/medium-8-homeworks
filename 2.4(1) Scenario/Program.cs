8cab36389ec88ab93114f9f5260430a2c12281e0
Саша Волков <Lexvolk2004@yandex.ru>
2019-09-10 23:52:59 +0400

    Adding task

---
+++ 2.4(1) Scenario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace _2._4_1__Scenario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScenarioReader scenarioReader = new ScenarioReader("Scenario");
+            while (true)
+            {
+                if (scenarioReader.CanReadDirectory())
+                {
+                    foreach (var line in scenarioReader.ReadNext())
+                        Console.WriteLine(line);
+                    Console.WriteLine();
+
+                    Thread.Sleep(500);
+                }
+            }
+        }
+    }
+}
---
+++ 2.4(1) Scenario/ScenarioReader.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using System.IO;
+
+namespace _2._4_1__Scenario
+{
+    class ScenarioReader
+    {
+        private string _directory;
+        private DirectoryInfo _directoryInfo;
+
+        private FileInfo[] _lastFileInfos;
+        private int _currentFileIndex;
+
+        public ScenarioReader(string directory)
+        {
+            _directory = directory;
+            _directoryInfo = new DirectoryInfo(_directory);
+            _lastFileInfos = GetFileInfos();
+        }
+
+        public string[] ReadNext()
+        {
+            if (!CanReadDirectory())
+                return null;
+
+            if (WereFilesChanged())
+            {
+                _lastFileInfos = GetFileInfos();
+                _currentFileIndex = 0;
+            }
+
+            string[] result = File.ReadAllLines(_lastFileInfos[_currentFileIndex].FullName);
+
+            _currentFileIndex++;
+            if (_currentFileIndex >= _lastFileInfos.Length)
+                _currentFileIndex = 0;
+
+            return result;
+        }
+
+        public bool CanReadDirectory() => GetFileInfos().Length != 0;
+
+        private bool WereFilesChanged()
+        {
+            FileInfo[] newFilesInfos = GetFileInfos();
+
+            if (_lastFileInfos.Length != newFilesInfos.Length)
+                return true;
+
+            for (int i = 0; i < newFilesInfos.Length; i++)
+            {
+                if (_lastFileInfos[i].Name != newFilesInfos[i].Name)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private FileInfo[] GetFileInfos() => _directoryInfo.GetFiles().Where(file => file.Name.Any(symbol => char.IsDigit(symbol))).ToArray();
+    }
+}
